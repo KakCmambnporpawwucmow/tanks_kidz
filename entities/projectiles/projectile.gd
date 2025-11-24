b70ffd1a6988af89f7fa1e62c814da1910e0b8bc
KakCmambnporpawwucmow <kak_cmamb_nporpawwucmow@mail.ru>
2025-11-24 14:21:57 +0300

    UPDATE
    Упрощен снаряд и производные типы, убран рикошет и увеличение брони от угла при пробитии.

--- entities/projectiles/projectile.gd
+++ entities/projectiles/projectile.gd
@@ -4,81 +4,47 @@ class_name Projectile
 
 @export_group("Projectile Settings")
 @export var initial_speed: float = 300.0
-@export var show_trajectory: bool = true  # Показывать ли траекторию для отладки
-
-@export_group("Dependencies")
-@export var armor_component: ArmorComponent = null
-@export var health_component: HealthComponent = null
-
-@export_group("Projectile describe")
-@export var describe:String
+@export var damage:int = 100
+@export var min_speed:int = 100
+@export var armor_penetration:int = 50
 
 @onready var visible_notifier = $VisibleOnScreenNotifier2D
 
-var _start_position: Vector2
-var _distance_traveled: float = 0.0
-var _original_direction: Vector2  # Исходное направление без рассеивания
-var _damage:int = 0
-var _owner_tank:Tank = null
-
 func _ready():
-	assert(armor_component != null, "Projectile: ArmorComponent must be assigned")
-	assert(health_component != null, "Projectile: HealthComponent must be assigned")
+	set_physics_process(false)
 	$PenetrationMarker.visible = false
-	
-func set_owner_tank(tank:Tank):
-	_owner_tank = tank
-	
-func get_owner_tank()->Tank:
-	return _owner_tank
-
-func set_damage(damage:int):
-	_damage = damage
 	
 func activate(fire_position: Vector2, fire_direction: Vector2):
 	global_position = fire_position
 	global_rotation = fire_direction.angle()
-	_start_position = global_position
-	_original_direction = fire_direction
-	
 	linear_velocity = fire_direction * initial_speed
-	
-	# Визуализация траектории для отладки
-	if show_trajectory and Engine.is_editor_hint():
-		draw_trajectory_debug()
+	set_physics_process(true)
 
-func on_death():
+func _physics_process(_delta: float) -> void:
+	if linear_velocity.length() < min_speed:
+		on_death()
+
+func on_death(_damage:int = 0):
 	set_physics_process(false)
 	linear_velocity = Vector2.ZERO
 	$view.visible = false
 	create_destruction_effects()
 	if _damage > 0:
-		$PenetrationMarker/Label.text = str(_damage)
+		$PenetrationMarker/Label.text = str(damage)
 		$AnimationPlayer.play("view_damage")
 	else:
 		queue_free()
 
 func create_destruction_effects():
-	print("Projectile destroyed at distance: ", _distance_traveled)
-
-# Визуализация траектории для отладки рассеивания
-func draw_trajectory_debug():
-	var line = Line2D.new()
-	line.width = 2
-	line.default_color = Color.YELLOW
-	line.add_point(Vector2.ZERO)
-	line.add_point(_original_direction * 100)
-	get_parent().add_child(line)
+	pass
 
 func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
 	queue_free()
 
-func _on_armor_component_damage_done(damage: int) -> void:
-	_damage = damage
-
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
 
-func _on_armor_component_armor_ricochet(hit_angle: float) -> void:
-	#linear_velocity = linear_velocity * hit_angle
-	pass
+func _on_body_entered(body: Node) -> void:
+	if body is ArmorComponent:
+		if body.check_penetration(armor_penetration, damage):
+			on_death(damage)
