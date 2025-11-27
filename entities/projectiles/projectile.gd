4bec7ba02eaff90f4775a28f9d0e67ec296a97f1
KakCmambnporpawwucmow <kak_cmamb_nporpawwucmow@mail.ru>
2025-11-27 17:01:01 +0300

    UPDATE
    Снаряд теперь сам обрабатывает попадание и нанесение урона игровому объекту.

--- entities/projectiles/projectile.gd
+++ entities/projectiles/projectile.gd
@@ -35,9 +35,13 @@ func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
 
-func _on_body_entered(body: Node) -> void:
-	if body is ArmorComponent:
-		if body.check_penetration(armor_penetration, damage):
-			on_death(damage)
-		else:
-			$AnimationPlayer.play("ricoshet")
+func _on_body_shape_entered(body_rid: RID, body: Node, body_shape_index: int, local_shape_index: int) -> void:
+	if body is Tank:
+		var body_shape_owner_id = body.shape_find_owner(body_shape_index)
+		if body_shape_owner_id != -1:
+			var body_shape = body.shape_owner_get_owner(body_shape_owner_id).shape
+			if body_shape is RectangleShape2D and armor_penetration >= min(body_shape.size.x, body_shape.size.y): 
+				body.health_component.take_damage(damage)
+				on_death(damage)
+			else:
+				$AnimationPlayer.play("ricoshet")
