8270cda7638b58626dd8c54e9b0e1e9fa91f8e57
KakCmambnporpawwucmow <kak_cmamb_nporpawwucmow@mail.ru>
2025-11-30 00:41:56 +0300

    UPDATE
    Команда TankRotateTurretCommand заменена на RotateToCommand, так как управляться может не только танк.

--- command/RotateToCommand.gd
+++ command/RotateToCommand.gd
@@ -1,18 +1,17 @@
 # commands/tank_commands.gd
-class_name TankRotateTurretCommand
+class_name RotateToCommand
 extends Command
 
 var target_pos: Vector2
 
 func init(pos: Vector2) -> Command:
-	entity_id = "TankRotateTurretCommand"
+	entity_id = "RotateToCommand"
 	timestamp = Time.get_ticks_msec()
 	target_pos = pos
 	return self
 
 func execute(entity: Node) -> void:
-	if entity is Tank:
-		entity.rotating_turret_to(target_pos)
+		entity.rotating_to(target_pos)
 
 func serialize() -> Dictionary:
 	var data = super()
