7c709cff8f397aaee83431ad5f7a240f77636a07
KakCmambnporpawwucmow <kak_cmamb_nporpawwucmow@mail.ru>
2025-11-30 00:50:40 +0300

    UPDATE
    Удалён флаг активации снаряда для ракеты. Теперь он не нужен так как снаряд создается перед закуском.

--- entities/projectiles/missile.gd
+++ entities/projectiles/missile.gd
@@ -3,19 +3,23 @@ class_name MissileProjectile
 
 @export var move_component:BaseMoveComponent = null
 
+func _ready() -> void:
+	for item in get_tree().get_nodes_in_group("CommandProducers"):
+		if item is CommandProducer:
+			if item.add_receiver(self) != true:
+				print("Error. Projectile {0}, not registred in CommandProducer".format([name]))
+
 func proc_command(command:Command):
-	if is_instance_valid(move_component) and is_activate:
+	if is_instance_valid(move_component):
 		command.execute(self)
 	
 func rotating_to(_position:Vector2):
 	move_component.smooth_look_at(_position)
 	
 func _process(delta: float) -> void:
-	if is_activate:
-		move_component.move(Vector2.RIGHT)
+	move_component.move(Vector2.RIGHT)
 		
 func on_death(_damage:int = 0):
-	is_activate = false
 	move_component.move(Vector2.ZERO)
 	super.on_death(_damage)
 
