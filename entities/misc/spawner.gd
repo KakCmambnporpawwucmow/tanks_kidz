cb88190dd4bd18e3f4217441f8a46becda0b35b1
KakCmambnporpawwucmow <igrovoy.club@bk.ru>
2026-01-04 00:07:47 +0300

    FIX. Исправлена ошибка обращения к невалидному узлу в Spawner.

--- entities/misc/spawner.gd
+++ entities/misc/spawner.gd
@@ -1,11 +1,15 @@
 extends Node2D
 
+class_name Spawner
+
 @export_group("Tank")
 @export var tank_ps:PackedScene = null
 @export var spawn_count:int = 1
+@export var player_command:Tank.EPlayers = Tank.EPlayers.ENEMY
 @export_group("Tank manual")
 @export var command_producer:CommandProducer = null
 @export var ammo_stat:AmmoStatistic = null
+@export var health_bar_stat:HealthBar = null
 @export_group("Tank camera")
 @export var limit_top:int = 0
 @export var limit_bottom:int = 0
@@ -18,16 +22,20 @@ func _ready() -> void:
 	
 func _on_child_exiting_tree(node: Node) -> void:
 	Logi.info("Spawn: remove tank {0} from layer".format([node.name]))
-	if spawn_count > 0:
+	if spawn_count > 0 and $Timer.is_inside_tree():
 		$Timer.start()
 
 func spawn():
 	if tank_ps != null and spawn_count > 0:
 		var new_tank = tank_ps.instantiate() as Tank
+		new_tank.set_player_command(player_command)
 		add_child(new_tank)
 		# инициализируем танк под управлением игрока с источником команд от UI и барами статы.
-		if ammo_stat != null and command_producer != null:
+		if ammo_stat != null:
 			ammo_stat.set_game_object(new_tank)
+		if health_bar_stat != null:
+			health_bar_stat.set_game_object(new_tank)	
+		if command_producer != null:
 			command_producer.add_receiver(new_tank)
 			var camera = Camera2D.new()
 			camera.limit_top = limit_top
